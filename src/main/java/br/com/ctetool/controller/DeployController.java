d7c1086fcc16ed4567b167cfac687814eff22ef3
tiago.rol <tiago.rol>
2016-06-19 14:36:15 -0300

    .

--- src/main/java/br/com/ctetool/controller/DeployController.java
+++ src/main/java/br/com/ctetool/controller/DeployController.java
@@ -1,8 +1,11 @@
 package br.com.ctetool.controller;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -22,6 +25,8 @@ import br.com.ctetool.service.ConfigurationService;
 
 @Controller
 public class DeployController {
+	
+	private static final Logger logger = Logger.getLogger(DeployController.class);
 	
 	private static final String SO_WINDOWS = "WINDOWS";
 
@@ -45,13 +50,17 @@ public class DeployController {
 
 	@RequestMapping("deploy")
 	public ModelAndView deploy(@RequestParam long id) throws IOException{
+		logger.info("Buscando Benchmark com Id: " + id + "...");
 		Benchmark benchmark = benchmarkService.fetchById(id);
 		
+		logger.info("Alterando inputs.yaml");
 		ClassLoader classLoader = getClass().getClassLoader();
 		File inputs = new File(classLoader.getResource("templateInputs.yaml").getFile());
 		String contentsAsString = FileUtils.getContentsAsString(inputs);
 		contentsAsString = replaceValues(contentsAsString, benchmark);
 		FileCopyUtils.copy(contentsAsString.getBytes(), new File(pathFileInputs));
+		
+		logger.info("Atualizando status do Benchmark...");
 		benchmark.setStatus(Status.WAITING);
 		benchmarkService.updateStatus(benchmark);
 		run(benchmark);
@@ -71,16 +80,33 @@ public class DeployController {
 		contentsAsString = contentsAsString.replace("#{rounds}", benchmark.getRounds().toString());
 		contentsAsString = contentsAsString.replace("#{workloads}", benchmark.getWorkloads());
 		
+		logger.info("Conteudo inputs.yaml");
+		logger.info(contentsAsString);
 		return contentsAsString;
 	}
 	
 	private void run(Benchmark benchmark) throws IOException{
+		logger.info("Inicio da execucao do Script...");
 		if(isWindows()){
+			logger.info("SO Windows...");
 			Runtime.getRuntime().exec("cmd /c start " + pathFileScript + getNameFileScript(benchmark));
 		}else{
-			ProcessBuilder pb = new ProcessBuilder(pathFileScript + getNameFileScript(benchmark));
-			pb.start();
+			logger.info("SO Linux...");
+			ProcessBuilder pb = new ProcessBuilder(pathFileScript + getNameFileScript(benchmark)).inheritIO();
+			logConsole(pb);
 		}
+	}
+	
+	private void logConsole(ProcessBuilder process) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(process.start().getInputStream()));
+		StringBuilder builder = new StringBuilder();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			builder.append(line);
+			builder.append(System.getProperty("line.separator"));
+		}
+		String result = builder.toString();
+		logger.info(result);
 	}
 
 	private String getNameFileScript(Benchmark benchmark) {
